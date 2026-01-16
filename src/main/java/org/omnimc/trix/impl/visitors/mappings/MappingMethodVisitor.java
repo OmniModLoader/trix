d984043feac5b54a0a91deea60d70054b9123eff
Caden <cadencasteel10@gmail.com>
2026-01-15 23:16:41 -0600

    Updated License

--- src/main/java/org/omnimc/trix/impl/visitors/mappings/MappingMethodVisitor.java
+++ src/main/java/org/omnimc/trix/impl/visitors/mappings/MappingMethodVisitor.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2024-2025 OmniMC
+ * Copyright (c) 2024-2026 OmniMC
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -20,6 +20,7 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
+ *
  */
 
 package org.omnimc.trix.impl.visitors.mappings;
